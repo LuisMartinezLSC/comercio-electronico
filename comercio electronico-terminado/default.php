9f5d721acf7a3bd7cde0e80f1d100dcdce05835d
LuisMartinezLSC <jose_luisLSC@hotmail.com>
2017-03-20 00:37:57 -0600

    Add files via upload

---
+++ comercio electronico-terminado/default.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+	<head lang="es">
+	 <meta charset="UTF-8"/>
+		<title> Comercio Electronico </title>
+		<link rel="stylesheet" href="css/menu.css">
+		<link rel="stylesheet" href="css/fontello.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="arriba.js"></script>
+		
+</head>
+	<body>
+			
+	
+		<center><header><div id="vacio">
+		<div class="barra">
+				<nav>
+				<ul>
+					<li><a href="default.php" class="icon icon-dashcube">Inicio<span class="icon icon-angle-up"></a></li>
+					<li><a href="origen.html">Origen<span class="icon icon-angle-up"></a></li>
+					<li><a href="ventajas.html">Ventajas<span class="icon icon-angle-up"></a>
+					 <ul>
+					  <li><a href="desventaja.html">Desventajas</a></li>
+					 </ul>
+					</li>
+					<li><a href="infrestructura.html">Infrestructura <span class="icon icon-angle-up"></a></li>
+					<li><a href="referencia.html">Referencia <span class="icon icon-angle-up"></a></li>
+				 </ul>
+		</div>		
+		</header></center>
+	<br>
+	<br>
+	<br>
+	
+		<header>
+					<div class="logo">
+					<center><img src="http://www.comercioelectronico.com.uy/artworks/artworks_default13_comercioelectronico/logo.png" alt="" width="900px"><center>
+					</div>
+				</header>
+		
+		
+</div>
+	<div class="social">
+		<ul>
+
+		<li><a href="http://www.facebook.com" target="_black" class="icon-facebook"></a></li>
+		<li><a href="http://www.twitter.com" target="_black" class="icon-twitter"></a></li>
+		<li><a href="http://www.github.com/LuisMartinezLSC" target="_black" class="icon-github"></a></li>
+		<li><a href="http://www.youtube.com" target="_black" class="icon-youtube-play"></a></li>
+		</ul>
+		</div>
+
+	<div id="body">
+	
+	<!--		<div class="banner">
+			
+			</div> -->
+	
+	
+	
+	<tr>
+	
+	
+	
+	
+			<center><H1 class="titulo">¿Que es el comercio?</h1><center><br><br>
+			<td>
+			<h3 align="JUSTIFY">Realización de transacciones mediante medios electrónicos, como por ejemplo el intercambio electrónico de datos.
+			Originalmente el término se aplicaba a la realización de <strong>transacciones</strong> 
+			mediante medios electrónicos, como por ejemplo el <strong>intercambio electrónico de datos</strong>.
+			Sin embargo, con el advenimiento de la <strong>Internet</strong> a mediados de los años 90, 
+			comenzó el concepto de venta de servicios por la red, usando como forma de pago medios electrónicos 
+			como las tarjetas de crédito. Desde entonces, las empresas han estado preocupadas por mantenerse a la 
+			vanguardia y poder ofrecer lo que hoy en día sus clientes están demandando.</h3>
+			<br><br>				
+			<br><br><br>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/mLibPKHUVsE?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+			
+			</tr>
+
+	</div>
+
+
+		<table align="center" style="width:100%; height:100px; "bgcolor="E3E3E3" ><!-- pie de pagina -->
+
+		<!--pie de pagina -->
+		    <tr>
+			<td bgcolor="#DBDBDB">
+			<font color="black">
+				<center><p>
+				<b> <span class=" icon-circle-notch">UNIVERSIDAD AUTONOMA DE CHIAPAS</span></b><br>
+				<b><span class=" icon-street-view">Martínez Pérez José Luis</span></b>
+				</p></center>
+
+			</tr>
+			</td>
+
+	<span class="ir-arriba  icon-angle-up"></span>
+
+
+
+	</body>
+
+
+</html>
